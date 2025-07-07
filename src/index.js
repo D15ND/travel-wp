900a07f8a8eef2653fd3c71ae3b3e598dadea1c5
ILYA <106765406+D15ND@users.noreply.github.com>
2025-07-07 23:25:35 +0300

    feat: add webpack structure

---
+++ src/index.js
@@ -0,0 +1,2 @@
+console.log('hello with webpack')
+console.log('test js changes')
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'development', // Или 'production'
+  entry: './src/index.js', // Точка входа
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true, // Очищает папку dist перед сборкой
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // Обработка JS-файлов
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',  // Добавляем babel
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/, // Обработка CSS
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i, // Загрузка изображений
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // Шаблон HTML
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'src/assets', to: 'assets' },
+      ],
+    }),
+  ],
+  devServer: {
+    static: './dist',
+    port: 3456,
+    open: true,
+    hot: true,
+    liveReload: true,   // автообновление страницы браузера
+    watchFiles: ['src/**/*']  // отслеживает обновление не только js но и css & sass
+  },
+  devtool: 'source-map', // Для отладки
+};
